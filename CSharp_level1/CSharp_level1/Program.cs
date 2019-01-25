a71627d9c4a3812c4df92416766c39f47b2cb066
VictorCher <192.168.127.1@mail.ru>
2019-01-25 12:56:51 +0300

    Сделал задания 1, 2, 3, 4, 7

--- CSharp_level1/CSharp_level1/Program.cs
+++ CSharp_level1/CSharp_level1/Program.cs
@@ -1,4 +1,4 @@
-﻿// Чернышов Виктор. Урок 1
+﻿// Чернышов Виктор. Урок 2
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,81 +11,86 @@ namespace CSharp_level1
     {
         static void Task1()
         {
-            /* Написать программу «Анкета». Последовательно задаются вопросы (имя, фамилия, возраст, рост, вес). 
-             * В результате вся информация выводится в одну строчку:
-             * а) используя  склеивание;
-             * б) используя форматированный вывод;
-             * в) используя вывод со знаком $. */
-
-            Console.WriteLine("Анкета");
-            Console.Write("Введите Ваше имя: ");
-            string first_name = Console.ReadLine();
-            Console.Write("Введите Вашу фамилию: ");
-            string last_name = Console.ReadLine();
-            Console.Write("Введите Ваш возраст: ");
-            string age = Console.ReadLine();
-            Console.Write("Введите Ваш рост: ");
-            string height = Console.ReadLine();
-            Console.Write("Введите Ваш вес: ");
-            string weight = Console.ReadLine();
-            Console.WriteLine("Ваше имя - " + first_name + ", фамилия - " + last_name + ", возраст - " + age + ", рост - " + height + ", вес - " + weight);
-            Console.WriteLine("Ваше имя - {0}, фамилия - {1}, возраст - {2}, рост - {3}, вес - {4}", first_name, last_name, age, height, weight);
-            Console.WriteLine($"Ваше имя - {first_name}, фамилия - {last_name}, возраст - {age}, рост - {height}, вес - {weight}");
+            // Написать метод, возвращающий минимальное из трёх чисел.
+            Console.Write("Введите первое число: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Введите третье число: ");
+            int c = int.Parse(Console.ReadLine());
+            int min;
+            if (a < b) min = a < c ? a : c;
+            else min = b < c ? b : c;
+            Console.WriteLine($"Минимальным из этих трех чисел является: {min}");
         }
         static void Task2()
         {
-            /* Ввести вес и рост человека. Рассчитать и вывести индекс массы тела (ИМТ) по формуле k=m/(h*h); 
-             * где m — масса тела в килограммах, h — рост в метрах. */
-            Console.Write("Введите массу тела в килограммах: ");
-            float weight = float.Parse(Console.ReadLine());
-            Console.Write("Введите рост в метрах: ");
-            float height = float.Parse(Console.ReadLine());
-            float k = weight / (height * height);
-            Console.WriteLine($"Индекс массы тела составляет: {k:N}");
+            // Написать метод подсчета количества цифр числа.
+            Console.Write("Введите число: ");
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            while (n != 0)
+            {
+                n /= 10;
+                count++;
+            }
+            Console.WriteLine($"Количество цифр: {count}");
         }
-        static double Task3(int x1, int y1, int x2, int y2)
+        static void Task3()
         {
-            /* Написать программу, которая подсчитывает расстояние между точками с координатами x1, y1 и x2,y2 по формуле 
-             * r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)).Вывести результат, используя спецификатор формата .2f
-             * (с двумя знаками после запятой). Выполнить задание, оформив вычисления расстояния между точками в виде метода. */
-            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            /* С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечётных
+             * положительных чисел. */
+            int sum = 0;
+            int n = 0;
+            do
+            {
+                Console.Write("Введите число: ");
+                n = int.Parse(Console.ReadLine());
+                if(n > 0 && n % 2 != 0) sum += n;
+            }
+            while (n!=0);
+            Console.WriteLine("Сумм всех нечётных положительных чисел равна: " + sum);
         }
-        static void Task4(ref int a, ref int b)
+        static bool Task4()
         {
-            /* Написать программу обмена значениями двух переменных:
-             * а) с использованием третьей переменной;
-             * б) без использования третьей переменной. */
-            int buf = a;
-            a = b;
-            b = buf;
-            /*
-            a ^= b;
-            b ^= a;
-            a ^= b;
-            */
+            /* Реализовать метод проверки логина и пароля. На вход метода подается логин и пароль. На
+             * выходе истина, если прошел авторизацию, и ложь, если не прошел (Логин: root, Password:
+             * GeekBrains). Используя метод проверки логина и пароля, написать программу: пользователь
+             * вводит логин и пароль, программа пропускает его дальше или не пропускает. С помощью
+             * цикла do while ограничить ввод пароля тремя попытками. */
+            int count = 0;
+            Console.WriteLine("Требуется авторизация");
+            do
+            {
+                Console.Write("Логин: ");
+                string log = Console.ReadLine();
+                Console.Write("Пароль: ");
+                string pas = Console.ReadLine();
+                if (log == "root" && pas == "GeekBrains") return true;
+                Console.WriteLine("Отказано в доступе");
+                count++;
+            }
+            while (count<3);
+            return false;
+        }
+        static int Task7(int a, int b)
+        {
+            /* a) Разработать рекурсивный метод, который выводит на экран числа от a до b(a<b).
+             * б) *Разработать рекурсивный метод, который считает сумму чисел от a до b. */
+            Console.Write(a + " ");
+            if (a < b) a += Task7(a + 1, b);
+            return a;
         }
         static void Main(string[] args)
         {
             // Task1();
             // Task2();
-            /*
-            Console.Write("Введите координату Х для первой точки: ");
-            int x1 = int.Parse(Console.ReadLine());
-            Console.Write("Введите координату Y для первой точки: ");
-            int y1 = int.Parse(Console.ReadLine());
-            Console.Write("Введите координату Х для первой точки: ");
-            int x2 = int.Parse(Console.ReadLine());
-            Console.Write("Введите координату Y для первой точки: ");
-            int y2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Расстояние между точками равно: {0:n2}", Task3(x1, y1, x2, y2));
-            */
-
-            int a = 2;
-            int b = 3;
-            Task4(ref a, ref b);
-            Console.WriteLine(a + ", " + b);
-
-
+            // Task3();
+            // if (Task4()) Console.WriteLine("Авторизация прошла успешно");
+            int a = 1;
+            int b = 5;
+            Console.WriteLine($"\nСумма чисел от {a} до {b} равна {Task7(a, b)}");
+           
             Console.ReadLine();
         }
     }
