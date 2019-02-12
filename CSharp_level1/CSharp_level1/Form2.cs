5c57f89170542995732a176696b88fb35dcd4b14
VictorCher <192.168.127.1@mail.ru>
2019-02-12 17:00:38 +0300

    Сделал задание 1

---
+++ CSharp_level1/CSharp_level1/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_level1
+{
+    public partial class Form2 : Form
+    {
+        int count;
+        int target;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            target = rnd.Next(101);
+        }
+    }
+}
