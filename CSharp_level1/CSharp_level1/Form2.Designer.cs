3a46b9b3865734a4b7acd61bd90def595c8e3a0f
VictorCher <192.168.127.1@mail.ru>
2019-02-13 13:20:28 +0300

    Сделал все задания

--- CSharp_level1/CSharp_level1/Form2.Designer.cs
+++ CSharp_level1/CSharp_level1/Form2.Designer.cs
@@ -29,11 +29,13 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(96, 224);
+            this.button1.Location = new System.Drawing.Point(32, 88);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(96, 24);
             this.button1.TabIndex = 0;
@@ -41,13 +43,37 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(32, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(32, 88);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Ввести число";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 265);
+            this.ClientSize = new System.Drawing.Size(192, 159);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form2";
+            this.ShowIcon = false;
             this.Text = "Угадай число от 0 до 100";
             this.ResumeLayout(false);
 
@@ -56,5 +82,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
     }
 }
