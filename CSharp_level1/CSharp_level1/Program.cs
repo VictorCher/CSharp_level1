4d2af708826abbfa804c0306c4ed5d2d7b701787
VictorCher <192.168.127.1@mail.ru>
2019-02-14 16:45:16 +0300

    Сделал задание 1

--- CSharp_level1/CSharp_level1/Program.cs
+++ CSharp_level1/CSharp_level1/Program.cs
@@ -1,4 +1,4 @@
-﻿// Чернышов Виктор. Урок 7
+﻿// Чернышов Виктор. Урок 8
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,30 +10,21 @@ namespace CSharp_level1
 {
     class Program
     {
+        /// <summary>
+        /// C помощью рефлексии выводит все свойства структуры DateTime
+        /// </summary>
         static void Task1()
         {
-            /* а) Добавить в программу «Удвоитель» подсчёт количества отданных команд удвоителю.
-             * б) Добавить меню и команду «Играть». При нажатии появляется сообщение, какое число
-             * должен получить игрок. Игрок должен получить это число за минимальное количество ходов.
-             * в) *Добавить кнопку «Отменить», которая отменяет последние ходы. Используйте
-             * обобщенный класс Stack. */
-
-            Application.Run(new Form1());
+            DateTime dt = new DateTime();
+            object[] t = dt.GetType().GetProperties();
+            string result = "";
+            foreach (object a in t) result += a.ToString() + "\n";
+            MessageBox.Show(result, "Свойства структуры DataTime");
         }
-        static void Task2()
-        {
-            /* Используя Windows Forms, разработать игру «Угадай число». Компьютер загадывает число от
-             * 1 до 100, а человек пытается его угадать за минимальное число попыток. Компьютер говорит,
-             * больше или меньше загаданное число введенного.
-             * a) Для ввода данных от человека используется элемент TextBox;
-             * б) **Реализовать отдельную форму c TextBox для ввода числа. */
 
-            Application.Run(new Form2());
-        }
         public static void Main()
         {
-            //Task1();
-            Task2();
+            Task1();
 
         }
     }
